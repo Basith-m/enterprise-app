31597458d494d7eac3f692eb75d7e21b65890489
Basith-m <abm2k20@gmail.com>
2023-12-20 12:17:29 +0530

    header and sidebar completed

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
 import './App.css';
+import Header from './Components/Header/Header';
 import Dashboard from './Pages/Dashboard';
 
 function App() {
   return (
     <div className="App">
+      <Header />
       <Dashboard />
     </div>
   );
--- src/Pages/Dashboard.jsx
+++ src/Pages/Dashboard.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 import './Dashboard.css'
+import Sidebar from '../Components/Header/Sidebar/Sidebar'
 
 function Dashboard() {
   return (
-    <div>Dashboard</div>
+    <div className='container'>
+      <div className='side-bar'>
+        <Sidebar/>
+      </div>
+      <div className='body-container'>
+
+      </div>
+      <div className='right-icons'></div>
+    </div>
   )
 }
 
